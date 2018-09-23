74f626320ad9cda89603f50d65ef761d2c885f21
Federico Zarantonello <fedezara2001@gmail.com>
2018-09-23 22:59:40 +0200

    .

--- courseManager.php
+++ courseManager.php
@@ -61,7 +61,7 @@ function showCourses()
   <title>Pannello di amministrazione - Gestione Corsi</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/flatly/bootstrap.min.css">
   <link rel="stylesheet" href="styles/courseManager.css">
   <link rel="stylesheet" href="styles/general.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
